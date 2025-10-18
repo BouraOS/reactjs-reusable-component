b9cc5d395c33e83337d258a72044cdbb8f35ebdf
OBOURAMADA <OBOURAMADA@royalairmaroc.com>
2025-10-18 23:03:46 +0100

    Add isNotEmptyValue utility

--- src/utils/Validation.ts
+++ src/utils/Validation.ts
@@ -1,3 +1,21 @@
+export const isNotEmptyValue = (value: unknown): boolean => {
+  if (value === null || typeof value === "undefined") {
+    return false; // Covers null & undefined
+  } else if (typeof value === "number" && isNaN(value)) {
+    return false; // Covers NaN
+  } else if (typeof value === "string" && value.trim().length === 0) {
+    return false; // Empty or whitespace string
+  } else if (Array.isArray(value) && value.length === 0) {
+    return false; // Empty array
+  } else if (
+    typeof value === "object" &&
+    !(value instanceof Date) && // Exclude Date objects
+    Object.keys(value).length === 0
+  ) {
+    return false; // Empty object
+  }
+  return true; // Otherwise, return true
+};
 
 export function isEmptyObject(obj: any): boolean {
   // Check if the input is an object and not null
@@ -17,7 +35,8 @@ export const isUndefined = (value: any): boolean => value === undefined;
 
 export const isStrongPassword = (password: string): boolean => {
   // At least 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   return passwordRegex.test(password);
 };
 
